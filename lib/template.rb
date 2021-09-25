d1482615aad4e054419eb46ebe77cab728739703
Mauro Bosetti <mauro.bosetti@10pines.com>
2021-09-25 08:37:49 -0300

    Primera implementacion de un Trait que le da comportamiento a una clase

--- lib/template.rb
+++ lib/template.rb
@@ -1,3 +1,20 @@
-def trait(constante)
-  "Hola"
+class Trait
+  attr_reader :cosas
+
+  def initialize(cosas)
+    super()
+    @cosas = cosas
+  end
 end
+
+class Class
+  def uses(trait)
+    class_eval(&trait.cosas)
+  end
+end
+
+def trait(constante, &cosas)
+  Object.const_set(constante, Trait.new(cosas))
+end
+
+
--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -1,22 +1,22 @@
 require 'rspec'
 require 'template'
 
-class Prueba
-  # uses Dummy
+trait :Dummy do
+  def un_metodo_del_trait
+    10
+  end
 end
 
+class Prueba
+  uses Dummy
+end
 
 describe 'template tests' do
 
   it 'xd' do
-    trait :Dummy do
-      def un_metodo_del_trait
-        10
-      end
-    end
+    prueba = Prueba.new
 
-    expect(Prueba.new.respond_to?(:un_metodo_del_trait)).to be_truthy
-    expect(Prueba.new.un_metodo_del_trait).to be(10)
+    expect(prueba.respond_to?(:un_metodo_del_trait)).to be_truthy
+    expect(prueba.un_metodo_del_trait).to eq 10
   end
-
 end
