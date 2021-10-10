ab93673ed1237e3ae5a766328a3273cc5b9fcb06
cassa10 <cassanojoseluis@gmail.com>
2021-10-10 18:22:20 -0300

    fix comment

--- lib/operation_type.rb
+++ lib/operation_type.rb
@@ -9,7 +9,7 @@ def validate_operation_type(value)
   value
 end
 
-# value is true when is ':+' or ':-'
+# value is true when is ':+', ':-' or ':<<'
 def is_operation_type(value)
   OPERATION_TYPE.key?value
 end
