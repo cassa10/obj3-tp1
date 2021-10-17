ac3473c9869098072f64dea3918f246a567f787b
Mauro Bosetti <mauro.bosetti@10pines.com>
2021-10-16 21:07:04 -0300

    Agrega un test de combinacion de estrategias de conflictos

--- spec/trait_api_spec.rb
+++ spec/trait_api_spec.rb
@@ -674,6 +674,40 @@ describe 'Trait API' do
     expect(instancia.metodo_1(100)).to eq(230)
   end
 
+  it "cuando una clase implementa la composicion de dos traits con metodos conflictivos, puede usar una estrategia
+    para resolver conflictos con un metodo y una estrategia distinta para otro metodo" do
+    trait :UnTrait do
+      def metodo_1(n)
+        n + 10
+      end
+
+      def metodo_2
+        90
+      end
+    end
+
+    trait :OtroTrait do
+      def metodo_1(n)
+        n + 20
+      end
+
+      def metodo_2
+        190
+      end
+    end
+
+    class UnaClase
+      uses UnTrait + OtroTrait.on_conflict([
+                                             InjectReduce.new(:metodo_1, 0, proc { |acc, n| acc + n }),
+                                             CualquierImplementacion.new
+                                           ])
+    end
+
+    instancia = UnaClase.new
+    expect(instancia.metodo_1(100)).to eq(230)
+    expect(instancia.metodo_2 == 90 || instancia.metodo_2 == 190).to be_truthy
+  end
+
   it 'cuando se le pide la description a un trait, luego este devuelve su nombre junto a el conjunto de operaciones aplicado' do
     trait :UnTrait do
       def m1
